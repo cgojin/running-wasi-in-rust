8b0f88043462c99c97f1f57cc9577a5a0a5e1b80
Jin Gong <gongjin21@hotmail.com>
2023-03-31 16:22:49 +0800

    Multi instances share data

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 use anyhow::Result;
 use wasmtime::*;
-use wasmtime_wasi::sync::WasiCtxBuilder;
+use wasmtime_wasi::{sync::WasiCtxBuilder, WasiCtx};
+
+struct State {
+    message: String,
+    wasi: WasiCtx,
+}
 
 fn main() -> Result<()> {
     // Define the WASI functions globally on the `Config`.
     let engine = Engine::default();
     let mut linker = Linker::new(&engine);
-    wasmtime_wasi::add_to_linker(&mut linker, |s| s)?;
+    wasmtime_wasi::add_to_linker(&mut linker, |state: &mut State| {
+        println!("Get WasiCtx, {}", state.message);
+        &mut state.wasi
+    })?;
 
     // Create a WASI context and put it in a Store; all instances in the store
     // share this context. `WasiCtxBuilder` provides a number of ways to
@@ -15,15 +23,42 @@ fn main() -> Result<()> {
         .inherit_stdio()
         .inherit_args()?
         .build();
-    let mut store = Store::new(&engine, wasi);
+    let mut store = Store::new(
+        &engine,
+        State {
+            message: String::from("all instances share this context and message"),
+            wasi,
+        },
+    );
 
-    // Instantiate our module with the imports we've created, and run it.
+    // Load module
     let module = Module::from_file(&engine, "target/wasm32-wasi/debug/wasi.wasm")?;
+
+    // Instantiate module
     linker.module(&mut store, "", &module)?;
+
+    // Get the `default export`, it is `_start`, and run it
     linker
         .get_default(&mut store, "")?
         .typed::<(), ()>(&store)?
         .call(&mut store, ())?;
+
+    /*
+    // Get the `_start`, and run it
+    linker
+        .get(&mut store, "", "_start")
+        .unwrap()
+        .into_func()
+        .unwrap()
+        .typed::<(), ()>(&mut store)?
+        .call(&mut store, ())?;
+
+    // Get the `_start`, and run it
+    let instance = linker.instantiate(&mut store, &module)?;
+    instance
+        .get_typed_func::<(), ()>(&mut store, "_start")?
+        .call(&mut store, ())?;
+    */
 
     Ok(())
 }
